a3f2457eb24219a9b2738faa8a9d67d0aa064ee3
Kelechi194 <lonpah9@gmail.com>
2024-08-07 12:47:05 +0100

    Created calculator web page using html, css and php

---
+++ Day1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    echo "<center><h2>HELLO WORLD!</h2><center>";
+
+    ?>
+
+
+</body>
+</html>
